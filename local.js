f6c461a97aae04b787b48fa12a86dd78db667c36
abdur rahman <rsaj53@gmail.com>
2025-03-21 03:29:38 +0100

    first commit

--- local.js
+++ local.js
@@ -1,46 +1,49 @@
-const savedata=()=>{
-    const productField=document.getElementById('product');
-    const quantityField=document.getElementById('quantity');
-    const product=productField.value;
-    const quantity=quantityField.value;
-    showdata(product,quantity);
-    addproduct(product,quantity)
-productField.value='';
-quantityField.value='';
-}
+const people = [
+    { firstName: 'Sam', lastName: 'Hughes', DOB: '07/07/1978', department: 'Development', salary: '45000' },
+    { firstName: 'Terri', lastName: 'Bishop', DOB: '02/04/1989', department: 'Development', salary: '35000' },
+    { firstName: 'Jar', lastName: 'Burke', DOB: '11/01/1985', department: 'Marketing', salary: '38000' },
+    { firstName: 'Julio', lastName: 'Miller', DOB: '12/07/1991', department: 'Sales', salary: '40000' },
+    { firstName: 'Chester', lastName: 'Flores', DOB: '03/15/1988', department: 'Development', salary: '41000' },
+    { firstName: 'Madison', lastName: 'Marshall', DOB: '09/22/1980', department: 'Sales', salary: '32000' },
+    { firstName: 'Ava', lastName: 'Pena', DOB: '11/02/1986', department: 'Development', salary: '38000' },
+    { firstName: 'Gabriella', lastName: 'Steward', DOB: '08/26/1994', department: 'Marketing', salary: '46000' },
+    { firstName: 'Charles', lastName: 'Campbell', DOB: '09/04/1977', department: 'Sales', salary: '42000' },
+    { firstName: 'Tiffany', lastName: 'Lambert', DOB: '05/11/1990', department: 'Development', salary: '34000' },
+    { firstName: 'Antonio', lastName: 'Gonzalez', DOB: '03/24/1985', department: 'Office Management', salary: '49000' },
+    { firstName: 'Aaron', lastName: 'Garrett', DOB: '09/04/1985', department: 'Development', salary: '39000' },
+];
+// Exercises
 
- const showdata=(product,quantity)=>{
-    const dataField=document.getElementById('dataAshbe')
-    const ul=document.createElement('ul')
-    ul.innerHTML=`
-    <li> Product Name: ${product} = Product Quantity: ${quantity} </li>`
-    dataField.appendChild(ul)
+// 1) What is the average income of all the people in the array?
 
- }
 
- const getData=()=>{
-    let cart={};
-    const getStorage=localStorage.getItem('cart')
-    if(getStorage){
-        cart=JSON.parse(getStorage)
+// 2) Who are the people that are currently older than 30?
+
+
+// 3) Get a list of the people's full name (firstName and lastName).
+
+
+// 4) Get a list of people in the array ordered from youngest to oldest.
+
+
+// 5) How many people are there in each department?
+
+
+//Solution1:
+
+let total=0;
+const joma=people.map(person=>{
+   total=total+ parseInt(person['salary'])
+})
+total=parseInt(total/people.length)
+console.log(total)
+
+//solution2:
+const fullName=people.map(person=>{
+    const d = new Date().getFullYear()-new Date(person.DOB).getFullYear();
+    if(d>30){
+        console.log( person.firstName +' '+ person.lastName);
     }
-    return cart;
- }
-
- const addproduct=(product,quantity)=>{
-    const data=getData();
-    data[product]=quantity;
-    const dataStringify=JSON.stringify(data)
-    localStorage.setItem('cart',dataStringify)
- }
-
-const dataVisual=()=>{
-    const data=getData();
-    for(const product in data){
-        const quantity=data[product];
-        showdata(product,quantity)
-    }
-}
- dataVisual();
-
+ })
+ //solution3:
  
